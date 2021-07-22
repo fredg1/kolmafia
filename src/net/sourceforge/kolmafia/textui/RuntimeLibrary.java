3ec4a453605804f800b6ba2050700f4fbf79c1e0
fredg1 <gagnonfrederic9@gmail.com>
2021-07-22 00:18:48 -0400

    Same for everything *outside* of Parser, too

--- src/net/sourceforge/kolmafia/textui/RuntimeLibrary.java
+++ src/net/sourceforge/kolmafia/textui/RuntimeLibrary.java
@@ -3193,7 +3193,7 @@ public abstract class RuntimeLibrary
 	{
 		String name = null;
 
-		if ( value.getType() == DataTypes.INT_TYPE )
+		if ( value.getType().equals( DataTypes.INT_TYPE ) )
 		{
 			int num = (int) value.intValue();
 
