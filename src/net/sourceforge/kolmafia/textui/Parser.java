99baa50f4cf0d6514928b9cc6dcf0caf35b4ec1c
fredg1 <gagnonfrederic9@gmail.com>
2021-07-27 21:50:32 -0400

    delegate the making of new parsers to an overrideable method

--- src/net/sourceforge/kolmafia/textui/Parser.java
+++ src/net/sourceforge/kolmafia/textui/Parser.java
@@ -499,7 +499,7 @@ public class Parser
 		return this.importFile( fileName, scope, null );
 	}
 
-	public Scope importFile( final String fileName, final Scope scope, final Location location )
+	private Scope importFile( final String fileName, final Scope scope, final Location location )
 		throws InterruptedException
 	{
 		if ( Thread.interrupted() )
@@ -551,7 +551,7 @@ public class Parser
 
 		this.imports.put( scriptFile, scriptFile.lastModified() );
 
-		Parser parser = new Parser( scriptFile, null, this.imports );
+		Parser parser = this.getParser( scriptFile );
 		Scope result = parser.parseFile( scope );
 
 		for ( AshDiagnostic diagnostic : parser.diagnostics )
@@ -574,6 +574,11 @@ public class Parser
 		}
 
 		return result;
+	}
+
+	protected Parser getParser( final File scriptFile )
+	{
+		return new Parser( scriptFile, null, this.imports );
 	}
 
 	private Scope parseCommandOrDeclaration( final Scope result, final Type expectedType )
