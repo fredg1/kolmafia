a4b61ee3e1e75ff4f252adae34628c014814d28e
fredg1 <gagnonfrederic9@gmail.com>
2021-08-06 01:05:03 -0400

    split the inizialisation between the services

--- src/net/sourceforge/kolmafia/textui/langserver/workspaceservice/AshWorkspaceService.java
+++ src/net/sourceforge/kolmafia/textui/langserver/workspaceservice/AshWorkspaceService.java
@@ -35,6 +35,7 @@ package net.sourceforge.kolmafia.textui.langserver.workspaceservice;
 
 import org.eclipse.lsp4j.DidChangeConfigurationParams;
 import org.eclipse.lsp4j.DidChangeWatchedFilesParams;
+import org.eclipse.lsp4j.ServerCapabilities;
 import org.eclipse.lsp4j.services.WorkspaceService;
 
 import net.sourceforge.kolmafia.textui.langserver.AshLanguageServer;
@@ -47,6 +48,15 @@ public abstract class AshWorkspaceService
 	public AshWorkspaceService( final AshLanguageServer parent )
 	{
 		this.parent = parent;
+	}
+
+	public final void setCapabilities( final ServerCapabilities capabilities )
+	{
+		// executeCommandProvider
+
+		// workspaceSymbolProvider
+
+		// workspace
 	}
 
 	@Override
