123c753287e4c52900a303a7bd5e35e9d391df50
fredg1 <gagnonfrederic9@gmail.com>
2021-10-04 00:25:59 -0400

    documentation

--- src/net/sourceforge/kolmafia/textui/Parser.java
+++ src/net/sourceforge/kolmafia/textui/Parser.java
@@ -1290,6 +1290,10 @@ public class Parser
 
 		Token postVariableToken = this.currentToken();
 
+	/**
+	 * Parses the right-hand-side of a variable definition. It is assumed that the caller expects
+	 * an expression to be found, so this method never returns null.
+	 */
 	private Value parseInitialization( final Type t, final BasicScope scope )
 	{
 		Value result;
