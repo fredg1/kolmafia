75361f09c34b4c4602f6bf35f8f8d80e70826c2d
fredg1 <gagnonfrederic9@gmail.com>
2021-10-04 00:25:58 -0400

    better start using folders BEFORE this gets out of hand

--- src/net/sourceforge/kolmafia/textui/langserver/StateCheckWrappers.java
+++ src/net/sourceforge/kolmafia/textui/langserver/StateCheckWrappers.java
@@ -47,28 +47,28 @@ import org.eclipse.lsp4j.jsonrpc.messages.ResponseErrorCode;
  * <p>
  * Takes care of checking the server's state before every request and notification.
  */
-public class StateCheckWrappers
+abstract class StateCheckWrappers
 {
 	static class AshLanguageServer
 		extends net.sourceforge.kolmafia.textui.langserver.AshLanguageServer
 	{
-		private boolean notInitialized()
+		protected boolean notInitialized()
 		{
-			return this.state == ServerState.STARTED;
+			return this.getState() == ServerState.STARTED;
 		}
 
-		private boolean wasShutdown()
+		protected boolean wasShutdown()
 		{
-			return this.state == ServerState.SHUTDOWN;
+			return this.getState() == ServerState.SHUTDOWN;
 		}
 
 		/** To use with @JsonNotifications. They are simply ignored if we are not initialized. */
-		private boolean isActive()
+		protected boolean isActive()
 		{
-			return this.state == ServerState.INITIALIZED;
+			return this.getState() == ServerState.INITIALIZED;
 		}
 
-		private void initializeCheck()
+		protected void initializeCheck()
 		{
 			if ( this.notInitialized() )
 			{
@@ -77,7 +77,7 @@ public class StateCheckWrappers
 			}
 		}
 
-		private void shutdownCheck()
+		protected void shutdownCheck()
 		{
 			if ( this.wasShutdown() )
 			{
@@ -87,7 +87,7 @@ public class StateCheckWrappers
 		}
 
 		/** To use with @JsonRequests. They must throw an error if we are not initialized. */
-		private void stateCheck()
+		protected void stateCheck()
 		{
 			this.initializeCheck();
 			this.shutdownCheck();
@@ -129,154 +129,161 @@ public class StateCheckWrappers
 				super.cancelProgress( params );
 			}
 		}
+	}
 
 
-		// TextDocumentService
+	static class AshTextDocumentService
+		extends net.sourceforge.kolmafia.textui.langserver.textdocumentservice.AshTextDocumentService
+	{
+		AshTextDocumentService( final net.sourceforge.kolmafia.textui.langserver.AshLanguageServer parent )
+		{
+			super( parent );
+		}
 
 		@Override
 		public CompletableFuture<Either<List<CompletionItem>, CompletionList>> completion( CompletionParams position )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.completion( position );
 		}
 
 		@Override
 		public CompletableFuture<CompletionItem> resolveCompletionItem( CompletionItem unresolved )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.resolveCompletionItem( unresolved );
 		}
 
 		@Override
 		public CompletableFuture<Hover> hover( HoverParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.hover( params );
 		}
 
 		@Override
 		public CompletableFuture<SignatureHelp> signatureHelp( SignatureHelpParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.signatureHelp( params );
 		}
 
 		@Override
 		public CompletableFuture<Either<List<? extends Location>, List<? extends LocationLink>>> declaration( DeclarationParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.declaration( params );
 		}
 
 		@Override
 		public CompletableFuture<Either<List<? extends Location>, List<? extends LocationLink>>> definition( DefinitionParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.definition( params );
 		}
 
 		@Override
 		public CompletableFuture<Either<List<? extends Location>, List<? extends LocationLink>>> typeDefinition( TypeDefinitionParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.typeDefinition( params );
 		}
 
 		@Override
 		public CompletableFuture<Either<List<? extends Location>, List<? extends LocationLink>>> implementation( ImplementationParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.implementation( params );
 		}
 
 		@Override
 		public CompletableFuture<List<? extends Location>> references( ReferenceParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.references( params );
 		}
 
 		@Override
 		public CompletableFuture<List<? extends DocumentHighlight>> documentHighlight( DocumentHighlightParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.documentHighlight( params );
 		}
 
 		@Override
 		public CompletableFuture<List<Either<SymbolInformation, DocumentSymbol>>> documentSymbol( DocumentSymbolParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.documentSymbol( params );
 		}
 
 		@Override
 		public CompletableFuture<List<Either<Command, CodeAction>>> codeAction( CodeActionParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.codeAction( params );
 		}
 
 		@Override
 		public CompletableFuture<CodeAction> resolveCodeAction( CodeAction unresolved )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.resolveCodeAction( unresolved );
 		}
 
 		@Override
 		public CompletableFuture<List<? extends CodeLens>> codeLens( CodeLensParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.codeLens( params );
 		}
 
 		@Override
 		public CompletableFuture<CodeLens> resolveCodeLens( CodeLens unresolved )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.resolveCodeLens( unresolved );
 		}
 
 		@Override
 		public CompletableFuture<List<? extends TextEdit>> formatting( DocumentFormattingParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.formatting( params );
 		}
 
 		@Override
 		public CompletableFuture<List<? extends TextEdit>> rangeFormatting( DocumentRangeFormattingParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.rangeFormatting( params );
 		}
 
 		@Override
 		public CompletableFuture<List<? extends TextEdit>> onTypeFormatting( DocumentOnTypeFormattingParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.onTypeFormatting( params );
 		}
 
 		@Override
 		public CompletableFuture<WorkspaceEdit> rename( RenameParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.rename( params );
 		}
 
 		@Override
 		public CompletableFuture<LinkedEditingRanges> linkedEditingRange( LinkedEditingRangeParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.linkedEditingRange( params );
 		}
 
 		@Override
 		public void didOpen( DidOpenTextDocumentParams params )
 		{
-			if ( this.isActive() )
+			if ( this.parent.isActive() )
 			{
 				super.didOpen( params );
 			}
@@ -285,7 +292,7 @@ public class StateCheckWrappers
 		@Override
 		public void didChange( DidChangeTextDocumentParams params )
 		{
-			if ( this.isActive() )
+			if ( this.parent.isActive() )
 			{
 				super.didChange( params );
 			}
@@ -294,7 +301,7 @@ public class StateCheckWrappers
 		@Override
 		public void didClose( DidCloseTextDocumentParams params )
 		{
-			if ( this.isActive() )
+			if ( this.parent.isActive() )
 			{
 				super.didClose( params );
 			}
@@ -303,7 +310,7 @@ public class StateCheckWrappers
 		@Override
 		public void didSave( DidSaveTextDocumentParams params )
 		{
-			if ( this.isActive() )
+			if ( this.parent.isActive() )
 			{
 				super.didSave( params );
 			}
@@ -312,7 +319,7 @@ public class StateCheckWrappers
 		@Override
 		public void willSave( WillSaveTextDocumentParams params )
 		{
-			if ( this.isActive() )
+			if ( this.parent.isActive() )
 			{
 				super.willSave( params );
 			}
@@ -321,143 +328,150 @@ public class StateCheckWrappers
 		@Override
 		public CompletableFuture<List<TextEdit>> willSaveWaitUntil( WillSaveTextDocumentParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.willSaveWaitUntil( params );
 		}
 
 		@Override
 		public CompletableFuture<List<DocumentLink>> documentLink( DocumentLinkParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.documentLink( params );
 		}
 
 		@Override
 		public CompletableFuture<DocumentLink> documentLinkResolve( DocumentLink params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.documentLinkResolve( params );
 		}
 
 		@Override
 		public CompletableFuture<List<ColorInformation>> documentColor( DocumentColorParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.documentColor( params );
 		}
 
 		@Override
 		public CompletableFuture<List<ColorPresentation>> colorPresentation( ColorPresentationParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.colorPresentation( params );
 		}
 
 		@Override
 		public CompletableFuture<List<FoldingRange>> foldingRange( FoldingRangeRequestParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.foldingRange( params );
 		}
 
 		@Override
 		public CompletableFuture<Either<Range, PrepareRenameResult>> prepareRename( PrepareRenameParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.prepareRename( params );
 		}
 
 		@Override
 		public CompletableFuture<TypeHierarchyItem> typeHierarchy( TypeHierarchyParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.typeHierarchy( params );
 		}
 
 		@Override
 		public CompletableFuture<TypeHierarchyItem> resolveTypeHierarchy( ResolveTypeHierarchyItemParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.resolveTypeHierarchy( params );
 		}
 
 		@Override
 		public CompletableFuture<List<CallHierarchyItem>> prepareCallHierarchy( CallHierarchyPrepareParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.prepareCallHierarchy( params );
 		}
 
 		@Override
 		public CompletableFuture<List<CallHierarchyIncomingCall>> callHierarchyIncomingCalls( CallHierarchyIncomingCallsParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.callHierarchyIncomingCalls( params );
 		}
 
 		@Override
 		public CompletableFuture<List<CallHierarchyOutgoingCall>> callHierarchyOutgoingCalls( CallHierarchyOutgoingCallsParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.callHierarchyOutgoingCalls( params );
 		}
 
 		@Override
 		public CompletableFuture<List<SelectionRange>> selectionRange( SelectionRangeParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.selectionRange( params );
 		}
 
 		@Override
 		public CompletableFuture<SemanticTokens> semanticTokensFull( SemanticTokensParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.semanticTokensFull( params );
 		}
 
 		@Override
 		public CompletableFuture<Either<SemanticTokens, SemanticTokensDelta>> semanticTokensFullDelta( SemanticTokensDeltaParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.semanticTokensFullDelta( params );
 		}
 
 		@Override
 		public CompletableFuture<SemanticTokens> semanticTokensRange( SemanticTokensRangeParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.semanticTokensRange( params );
 		}
 
 		@Override
 		public CompletableFuture<List<Moniker>> moniker( MonikerParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.moniker( params );
 		}
+	}
 
 
-		// WorkspaceService
+	static class AshWorkspaceService
+		extends net.sourceforge.kolmafia.textui.langserver.workspaceservice.AshWorkspaceService
+	{
+		AshWorkspaceService( final net.sourceforge.kolmafia.textui.langserver.AshLanguageServer parent )
+		{
+			super( parent );
+		}
 
 		@Override
 		public CompletableFuture<Object> executeCommand( ExecuteCommandParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.executeCommand( params );
 		}
 
 		@Override
 		public CompletableFuture<List<? extends SymbolInformation>> symbol( WorkspaceSymbolParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.symbol( params );
 		}
 
 		@Override
 		public void didChangeConfiguration( DidChangeConfigurationParams params )
 		{
-			if ( this.isActive() )
+			if ( this.parent.isActive() )
 			{
 				super.didChangeConfiguration( params );
 			}
@@ -466,7 +480,7 @@ public class StateCheckWrappers
 		@Override
 		public void didChangeWatchedFiles( DidChangeWatchedFilesParams params )
 		{
-			if ( this.isActive() )
+			if ( this.parent.isActive() )
 			{
 				super.didChangeWatchedFiles( params );
 			}
@@ -475,7 +489,7 @@ public class StateCheckWrappers
 		@Override
 		public void didChangeWorkspaceFolders( DidChangeWorkspaceFoldersParams params )
 		{
-			if ( this.isActive() )
+			if ( this.parent.isActive() )
 			{
 				super.didChangeWorkspaceFolders( params );
 			}
@@ -484,14 +498,14 @@ public class StateCheckWrappers
 		@Override
 		public CompletableFuture<WorkspaceEdit> willCreateFiles( CreateFilesParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.willCreateFiles( params );
 		}
 
 		@Override
 		public void didCreateFiles( CreateFilesParams params )
 		{
-			if ( this.isActive() )
+			if ( this.parent.isActive() )
 			{
 				super.didCreateFiles( params );
 			}
@@ -500,14 +514,14 @@ public class StateCheckWrappers
 		@Override
 		public CompletableFuture<WorkspaceEdit> willRenameFiles( RenameFilesParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.willRenameFiles( params );
 		}
 
 		@Override
 		public void didRenameFiles( RenameFilesParams params )
 		{
-			if ( this.isActive() )
+			if ( this.parent.isActive() )
 			{
 				super.didRenameFiles( params );
 			}
@@ -516,14 +530,14 @@ public class StateCheckWrappers
 		@Override
 		public CompletableFuture<WorkspaceEdit> willDeleteFiles( DeleteFilesParams params )
 		{
-			this.stateCheck();
+			this.parent.stateCheck();
 			return super.willDeleteFiles( params );
 		}
 
 		@Override
 		public void didDeleteFiles( DeleteFilesParams params )
 		{
-			if ( this.isActive() )
+			if ( this.parent.isActive() )
 			{
 				super.didDeleteFiles( params );
 			}
---
+++ src/net/sourceforge/kolmafia/textui/langserver/textdocumentservice/AshTextDocumentService.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2005-2021, KoLmafia development team
+ * http://kolmafia.sourceforge.net/
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  [1] Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *  [2] Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *  [3] Neither the name "KoLmafia" nor the names of its contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION ) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE ) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.sourceforge.kolmafia.textui.langserver.textdocumentservice;
+
+import java.io.File;
+
+import java.util.List;
+
+import org.eclipse.lsp4j.DidChangeTextDocumentParams;
+import org.eclipse.lsp4j.DidCloseTextDocumentParams;
+import org.eclipse.lsp4j.DidOpenTextDocumentParams;
+import org.eclipse.lsp4j.DidSaveTextDocumentParams;
+import org.eclipse.lsp4j.TextDocumentContentChangeEvent;
+import org.eclipse.lsp4j.TextDocumentIdentifier;
+import org.eclipse.lsp4j.TextDocumentItem;
+import org.eclipse.lsp4j.VersionedTextDocumentIdentifier;
+import org.eclipse.lsp4j.services.TextDocumentService;
+
+import net.sourceforge.kolmafia.textui.langserver.AshLanguageServer;
+import net.sourceforge.kolmafia.textui.langserver.FilesMonitor;
+
+public abstract class AshTextDocumentService
+	implements TextDocumentService
+{
+	protected final AshLanguageServer parent;
+
+	public AshTextDocumentService( final AshLanguageServer parent )
+	{
+		this.parent = parent;
+	}
+
+	@Override
+	public void didOpen( DidOpenTextDocumentParams params )
+	{
+		this.parent.executor.execute( () -> {
+			TextDocumentItem document = params.getTextDocument();
+
+			File file = new File( FilesMonitor.sanitizeURI( document.getUri() ) );
+
+			this.parent.monitor.updateFile( file, document.getText(), document.getVersion() );
+		} );
+	}
+
+	@Override
+	public void didChange( DidChangeTextDocumentParams params )
+	{
+		this.parent.executor.execute( () -> {
+			VersionedTextDocumentIdentifier document = params.getTextDocument();
+			List<TextDocumentContentChangeEvent> changes = params.getContentChanges();
+
+			if ( changes.size() == 0 )
+			{
+				// Nothing to see here
+				return;
+			}
+
+			File file = new File( FilesMonitor.sanitizeURI( document.getUri() ) );
+
+			// We don't support incremental changes, so we expect the client
+			// to put the whole file's content in a single TextDocumentContentChangeEvent
+			this.parent.monitor.updateFile( file, changes.get( 0 ).getText(), document.getVersion() );
+		} );
+	}
+
+	@Override
+	public void didClose( DidCloseTextDocumentParams params )
+	{
+		this.parent.executor.execute( () -> {
+			TextDocumentIdentifier document = params.getTextDocument();
+
+			File file = new File( FilesMonitor.sanitizeURI( document.getUri() ) );
+
+			this.parent.monitor.updateFile( file, null, -1 );
+		} );
+	}
+
+	@Override
+	public void didSave( DidSaveTextDocumentParams params )
+	{
+		// TODO Auto-generated method stub
+		
+	}
+}
