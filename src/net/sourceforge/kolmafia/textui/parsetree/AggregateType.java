84b68a5e4acfd4371487a96e2645712f8e77d77e
fredg1 <gagnonfrederic9@gmail.com>
2021-10-04 20:18:22 -0400

    apply Spotless locally

--- src/net/sourceforge/kolmafia/textui/parsetree/AggregateType.java
+++ src/net/sourceforge/kolmafia/textui/parsetree/AggregateType.java
@@ -2,171 +2,147 @@ package net.sourceforge.kolmafia.textui.parsetree;
 
 import net.sourceforge.kolmafia.textui.DataTypes;
 
-public class AggregateType
-	extends CompositeType
-{
-	protected final Type dataType;
-	protected final Type indexType;
-	protected final boolean caseInsensitive;
-	protected int size;
+public class AggregateType extends CompositeType {
+  protected final Type dataType;
+  protected final Type indexType;
+  protected final boolean caseInsensitive;
+  protected int size;
 
-	private AggregateType( final String name, final Type dataType, final Type indexType, final int size, final boolean caseInsensitive )
-	{
-		super( name, DataTypes.TYPE_AGGREGATE );
-		this.dataType = dataType;
-		this.indexType = indexType;
-		this.size = size;
-		this.caseInsensitive = caseInsensitive && indexType.equals( DataTypes.STRING_TYPE );
-	}
+  private AggregateType(
+      final String name,
+      final Type dataType,
+      final Type indexType,
+      final int size,
+      final boolean caseInsensitive) {
+    super(name, DataTypes.TYPE_AGGREGATE);
+    this.dataType = dataType;
+    this.indexType = indexType;
+    this.size = size;
+    this.caseInsensitive = caseInsensitive && indexType.equals(DataTypes.STRING_TYPE);
+  }
 
-	public AggregateType( final AggregateType original )
-	{
-		this( "aggregate", original.dataType, original.indexType, original.size, original.caseInsensitive );
-	}
+  public AggregateType(final AggregateType original) {
+    this(
+        "aggregate",
+        original.dataType,
+        original.indexType,
+        original.size,
+        original.caseInsensitive);
+  }
 
-	// Map
-	public AggregateType( final Type dataType, final Type indexType )
-	{
-		this( "aggregate", dataType, indexType, -1, false );
-	}
+  // Map
+  public AggregateType(final Type dataType, final Type indexType) {
+    this("aggregate", dataType, indexType, -1, false);
+  }
 
-	// Map with case-insensitive string keys
-	public AggregateType( final Type dataType, final Type indexType, boolean caseInsensitive )
-	{
-		this( "aggregate", dataType, indexType, -1, caseInsensitive );
-	}
+  // Map with case-insensitive string keys
+  public AggregateType(final Type dataType, final Type indexType, boolean caseInsensitive) {
+    this("aggregate", dataType, indexType, -1, caseInsensitive);
+  }
 
-	// Array
-	public AggregateType( final Type dataType, final int size )
-	{
-		this( "aggregate", dataType, DataTypes.INT_TYPE, size, false );
-	}
+  // Array
+  public AggregateType(final Type dataType, final int size) {
+    this("aggregate", dataType, DataTypes.INT_TYPE, size, false);
+  }
 
-	// VarArg
-	public AggregateType( final String name, final Type dataType, final int size )
-	{
-		this( name, dataType, DataTypes.INT_TYPE, size, false );
-	}
+  // VarArg
+  public AggregateType(final String name, final Type dataType, final int size) {
+    this(name, dataType, DataTypes.INT_TYPE, size, false);
+  }
 
-	@Override
-	public Type getDataType()
-	{
-		return this.dataType;
-	}
+  @Override
+  public Type getDataType() {
+    return this.dataType;
+  }
 
-	@Override
-	public Type getDataType( final Object key )
-	{
-		return this.dataType;
-	}
+  @Override
+  public Type getDataType(final Object key) {
+    return this.dataType;
+  }
 
-	@Override
-	public Type getIndexType()
-	{
-		return this.indexType;
-	}
+  @Override
+  public Type getIndexType() {
+    return this.indexType;
+  }
 
-	@Override
-	public Value getKey( final Value key )
-	{
-		return key;
-	}
+  @Override
+  public Value getKey(final Value key) {
+    return key;
+  }
 
-	public int getSize()
-	{
-		return this.size;
-	}
+  public int getSize() {
+    return this.size;
+  }
 
-	public void setSize( final int size )
-	{
-		this.size = size;
-	}
+  public void setSize(final int size) {
+    this.size = size;
+  }
 
-	@Override
-	public boolean equals( Type o )
-	{
-		if ( o instanceof TypeReference )
-		{
-			o = ((TypeReference) o).getTarget();
-		}
+  @Override
+  public boolean equals(Type o) {
+    if (o instanceof TypeReference) {
+      o = ((TypeReference) o).getTarget();
+    }
 
-		return o instanceof AggregateType &&
-			this.dataType.equals( ( (AggregateType) o ).dataType ) &&
-			this.indexType.equals( ( (AggregateType) o ).indexType );
-	}
+    return o instanceof AggregateType
+        && this.dataType.equals(((AggregateType) o).dataType)
+        && this.indexType.equals(((AggregateType) o).indexType);
+  }
 
-	@Override
-	public Type simpleType()
-	{
-		if ( this.dataType instanceof AggregateType )
-		{
-			return this.dataType.simpleType();
-		}
-		return this.dataType;
-	}
+  @Override
+  public Type simpleType() {
+    if (this.dataType instanceof AggregateType) {
+      return this.dataType.simpleType();
+    }
+    return this.dataType;
+  }
 
-	@Override
-	public String toString()
-	{
-		return this.simpleType().toString() + " [" + this.indexString() + "]";
-	}
+  @Override
+  public String toString() {
+    return this.simpleType().toString() + " [" + this.indexString() + "]";
+  }
 
-	public String indexString()
-	{
-		if ( this.dataType instanceof AggregateType )
-		{
-			String suffix = ", " + ( (AggregateType) this.dataType ).indexString();
-			if ( this.size != -1 )
-			{
-				return this.size + suffix;
-			}
-			return this.indexType.toString() + suffix;
-		}
+  public String indexString() {
+    if (this.dataType instanceof AggregateType) {
+      String suffix = ", " + ((AggregateType) this.dataType).indexString();
+      if (this.size != -1) {
+        return this.size + suffix;
+      }
+      return this.indexType.toString() + suffix;
+    }
 
-		if ( this.size != -1 )
-		{
-			return String.valueOf( this.size );
-		}
-		return this.indexType.toString();
-	}
+    if (this.size != -1) {
+      return String.valueOf(this.size);
+    }
+    return this.indexType.toString();
+  }
 
-	@Override
-	public Value initialValue()
-	{
-		return  ( this.size != -1 ) ?
-			new ArrayValue( this ) :
-			new MapValue( this, this.caseInsensitive );
-	}
+  @Override
+  public Value initialValue() {
+    return (this.size != -1) ? new ArrayValue(this) : new MapValue(this, this.caseInsensitive);
+  }
 
-	@Override
-	public int dataValues()
-	{
-		if ( this.size <= 0 )
-		{
-			return -1;
-		}
-		int values = this.dataType.dataValues();
-		if ( values <= 0 )
-		{
-			return -1;
-		}
-		return this.size * values;
-	}
+  @Override
+  public int dataValues() {
+    if (this.size <= 0) {
+      return -1;
+    }
+    int values = this.dataType.dataValues();
+    if (values <= 0) {
+      return -1;
+    }
+    return this.size * values;
+  }
 
-	@Override
-	public boolean isBad()
-	{
-		return this.getIndexType().isBad() || this.getDataType().isBad();
-	}
+  @Override
+  public boolean isBad() {
+    return this.getIndexType().isBad() || this.getDataType().isBad();
+  }
 
-	public static class BadAggregateType
-		extends AggregateType
-		implements BadNode
-	{
-		/** More of a shortcut method than an actual class with a purpose... */
-		public BadAggregateType()
-		{
-			super( new BadType( null, null ), new BadType( null, null ) );
-		}
-	}
+  public static class BadAggregateType extends AggregateType implements BadNode {
+    /** More of a shortcut method than an actual class with a purpose... */
+    public BadAggregateType() {
+      super(new BadType(null, null), new BadType(null, null));
+    }
+  }
 }
--- src/net/sourceforge/kolmafia/textui/parsetree/FunctionList.java
+++ src/net/sourceforge/kolmafia/textui/parsetree/FunctionList.java
@@ -7,146 +7,118 @@ import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
-
 import org.eclipse.lsp4j.Location;
 
-public class FunctionList
-	extends SymbolList<Function>
-{
-	private final SortedMap<String,Function> list = new TreeMap<String, Function>();
-	private final Map<Function, List<Location>> references = new TreeMap<>( FUNCTION_COMPARATOR );
+public class FunctionList extends SymbolList<Function> {
+  private final SortedMap<String, Function> list = new TreeMap<String, Function>();
+  private final Map<Function, List<Location>> references = new TreeMap<>(FUNCTION_COMPARATOR);
 
-	// Assumes there will not be more than 65535 functions in any scope.
-	// Assumes that \0 will never appear in a function name.
-	private char sequence = '\0';
+  // Assumes there will not be more than 65535 functions in any scope.
+  // Assumes that \0 will never appear in a function name.
+  private char sequence = '\0';
 
-	public boolean add( final Function f )
-	{
-		this.list.put( f.getName().toLowerCase() + '\0' + this.sequence, f );
-		this.references.put( f, new ArrayList<>() );
-		++this.sequence;
-		return true;
-	}
+  public boolean add(final Function f) {
+    this.list.put(f.getName().toLowerCase() + '\0' + this.sequence, f);
+    this.references.put(f, new ArrayList<>());
+    ++this.sequence;
+    return true;
+  }
 
-	public boolean remove( final Function f )
-	{
-		return this.list.values().remove( f );
-	}
+  public boolean remove(final Function f) {
+    return this.list.values().remove(f);
+  }
 
-	public Function[] findFunctions( String name )
-	{
-		name = name.toLowerCase();
-		return this.list.subMap( name + '\0', name + '\1' ).values().toArray( new Function[ 0 ] );
-	}
+  public Function[] findFunctions(String name) {
+    name = name.toLowerCase();
+    return this.list.subMap(name + '\0', name + '\1').values().toArray(new Function[0]);
+  }
 
-	public boolean isEmpty()
-	{
-		return list.isEmpty();
-	}
+  public boolean isEmpty() {
+    return list.isEmpty();
+  }
 
-	public Iterator<Function> iterator()
-	{
-		return list.values().iterator();
-	}
+  public Iterator<Function> iterator() {
+    return list.values().iterator();
+  }
 
-	public boolean contains( final Function f )
-	{
-		return list.values().contains( f );
-	}
+  public boolean contains(final Function f) {
+    return list.values().contains(f);
+  }
 
-	public FunctionList clone()
-	{
-		FunctionList result = new FunctionList();
+  public FunctionList clone() {
+    FunctionList result = new FunctionList();
 
-		for ( Function type : this.list.values() )
-		{
-			result.add( type );
-		}
+    for (Function type : this.list.values()) {
+      result.add(type);
+    }
 
-		return result;
-	}
+    return result;
+  }
 
-	public void addReference( final Function function, final Location location )
-	{
-		List<Location> references = this.references.get( function );
+  public void addReference(final Function function, final Location location) {
+    List<Location> references = this.references.get(function);
 
-		if ( references != null )
-		{
-			references.add( location );
-		}
-	}
+    if (references != null) {
+      references.add(location);
+    }
+  }
 
-	public List<Location> getReferences( final Function function )
-	{
-		return this.references.get( function );
-	}
+  public List<Location> getReferences(final Function function) {
+    return this.references.get(function);
+  }
 
-	private static final Comparator<Function> FUNCTION_COMPARATOR =
-		( Function f1, Function f2 ) -> {
-			int comparison = f1.compareTo( f2 );
+  private static final Comparator<Function> FUNCTION_COMPARATOR =
+      (Function f1, Function f2) -> {
+        int comparison = f1.compareTo(f2);
 
-			if ( comparison != 0 )
-			{
-				return comparison;
-			}
+        if (comparison != 0) {
+          return comparison;
+        }
 
-			Iterator<VariableReference> f1Refs = f1.getVariableReferences().iterator();
-			Iterator<VariableReference> f2Refs = f2.getVariableReferences().iterator();
+        Iterator<VariableReference> f1Refs = f1.getVariableReferences().iterator();
+        Iterator<VariableReference> f2Refs = f2.getVariableReferences().iterator();
 
-			while ( f1Refs.hasNext() )
-			{
-				if ( !f2Refs.hasNext() )
-				{
-					return 1;
-				}
+        while (f1Refs.hasNext()) {
+          if (!f2Refs.hasNext()) {
+            return 1;
+          }
 
-				Type f1Type = f1Refs.next().getRawType();
-				Type f2Type = f2Refs.next().getRawType();
+          Type f1Type = f1Refs.next().getRawType();
+          Type f2Type = f2Refs.next().getRawType();
 
-				comparison = compareTypes( f1Type, f2Type );
+          comparison = compareTypes(f1Type, f2Type);
 
-				if ( comparison != 0 )
-				{
-					return comparison;
-				}
-			}
+          if (comparison != 0) {
+            return comparison;
+          }
+        }
 
-			if ( f2Refs.hasNext() )
-			{
-				return -1;
-			}
+        if (f2Refs.hasNext()) {
+          return -1;
+        }
 
-			return 0;
-		};
+        return 0;
+      };
 
-	private static final int compareTypes( final Type t1, final Type t2 )
-	{
-		int comparison = t1.getType() - t2.getType();
+  private static final int compareTypes(final Type t1, final Type t2) {
+    int comparison = t1.getType() - t2.getType();
 
-		if ( comparison != 0 )
-		{
-			return comparison;
-		}
+    if (comparison != 0) {
+      return comparison;
+    }
 
-		if ( t1 instanceof AggregateType )
-		{
-			comparison = compareTypes( ((AggregateType) t1).dataType, ((AggregateType) t2).dataType );
+    if (t1 instanceof AggregateType) {
+      comparison = compareTypes(((AggregateType) t1).dataType, ((AggregateType) t2).dataType);
 
-			if ( comparison == 0 )
-			{
-				comparison = compareTypes( ((AggregateType) t1).indexType, ((AggregateType) t2).indexType );
-			}
-		}
-		else if ( t1 instanceof RecordType )
-		{
-			comparison = t1.compareTo( t2 );
-		}
-		else if ( t1 instanceof TypeDef )
-		{
-			comparison = compareTypes( ((TypeDef) t1).base, ((TypeDef) t2).base );
-		}
+      if (comparison == 0) {
+        comparison = compareTypes(((AggregateType) t1).indexType, ((AggregateType) t2).indexType);
+      }
+    } else if (t1 instanceof RecordType) {
+      comparison = t1.compareTo(t2);
+    } else if (t1 instanceof TypeDef) {
+      comparison = compareTypes(((TypeDef) t1).base, ((TypeDef) t2).base);
+    }
 
-		return comparison;
-	}
+    return comparison;
+  }
 }
-
